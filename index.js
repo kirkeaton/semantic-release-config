784c80791a7f06086e334f23ea19e5fb01246c55
Kirk Eaton <contact@kirkeaton.ca>
2024-04-24 20:23:43 -0400

    fix: Disable comments and labels on released pull requests

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 module.exports = {
   branches: ['main'],
   ci: false,
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        failComment: false,
+        failTitle: false,
+        labels: false,
+        releasedLabels: false,
+        successComment: false,
+      },
+    ],
+  ],
 };
